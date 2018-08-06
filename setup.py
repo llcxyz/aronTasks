4ca44a85f0e91c024ef4db5bc2cac157a6865971
Aron Lau <llc_xyz@qq.com>
2018-08-07 00:27:55 +0800

    init
    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+__author__ = 'aron'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="aronTasks",
+    version="0.1",
+    description="Tasks Management",
+    author="llc_xyz@zhuc.net",
+    url="http://www.xunjiexidi.com",
+    license="XUNJIEXIDI",
+    packages=find_packages())
