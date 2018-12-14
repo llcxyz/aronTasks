92f40febd6395fd1df2446784fee99201f0ffdaa
Aron Lau <llc_xyz@qq.com>
2018-12-15 00:34:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-python Worker.py odoo.conf worker -Q campus.tasks.orm -n host1 --loglevel=DEBUG --concurrency=4
+python worker.py odoo.conf worker -Q campus.tasks.orm -n host1 --loglevel=DEBUG --concurrency=4
