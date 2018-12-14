3f0fb824997cc02e1643a98c48f3be574a944c08
Aron Lau <llc_xyz@qq.com>
2018-12-15 00:35:43 +0800

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,6 +1,11 @@
 # coding: utf-8
-broker_url = 'pyamqp://aron:aron@192.168.80.200'
+broker_url = 'redis://192.168.80.200:6379/0'
 task_serializer = 'json'
 task_routes = {'feed.tasks.import_feed': {'queue': 'feeds'}}
 task_ignore_result = False
 result_backend = 'redis://192.168.80.200:6379/1'
+enable_celery_task = True
+task_default_queue = 'aronTasks'
+app_name = 'aronTask'
+event_queue_prefix = "*"
+
